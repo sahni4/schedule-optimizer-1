e48e9a764735c7627a85279acac51df27a789558
Alpri Else <alprielse@gmail.com>
2017-10-16 19:26:37 -0500

    Fix styling

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,9 +4,9 @@ import App from './components/app';
 import Home from './components/views/home';
 import Contact from './components/views/contact';
 export default (
-  <Route path='/' component={App}>
-    <IndexRoute component={Home} />
-    <Route path='contact' component={Contact} />
-    <Route path='*' component={Home} />
-  </Route>
+    <Route path='/' component={App}>
+        <IndexRoute component={Home} />
+        <Route path='contact' component={Contact} />
+        <Route path='*' component={Home} />
+    </Route>
 );
